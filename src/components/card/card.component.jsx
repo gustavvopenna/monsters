6908eb50c257c863a1d633924fc12538888e7fa3
gustavvopenna <salvador@la-MacBook-Air-de-Gustavo.local>
2019-10-20 19:31:44 -0500

    Create CardsList, Card and SearchBox components

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './card.styles.css'
+
+export const Card = props => (
+  <div className='card-container'>
+    <h1 key={props.monster.id}>{props.monster.name}</h1>
+    <img
+      alt='monster'
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+    />
+  </div>
+)
